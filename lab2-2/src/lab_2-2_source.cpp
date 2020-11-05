41e3ec250feb10da65a386ece873a8f1ecef319f
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-05 20:58:07 +0800

    Capture video image and show on LCD

--- lab2-2/src/lab_2-2_source.cpp
+++ lab2-2/src/lab_2-2_source.cpp
@@ -17,7 +17,8 @@ struct framebuffer_info get_framebuffer_info ( const char *framebuffer_device_pa
 int main ( int argc, const char *argv[] )
 {
     // variable to store the frame get from video stream
-    cv::Mat frame;
+    cv::Mat frame, bgr565;
+    cv::Size2f frame_size;
 
     // open video stream device
     // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a5d5f5dacb77bbebdcbfb341e3d4355c1
@@ -25,37 +26,41 @@ int main ( int argc, const char *argv[] )
 
     // get info of the framebuffer
     // fb_info = ......
+    framebuffer_info fb_info = get_framebuffer_info("/dev/fb0");
 
     // open the framebuffer device
     // http://www.cplusplus.com/reference/fstream/ofstream/ofstream/
     // ofs = ......
+    std::ofstream ofs("/dev/fb0");
 
     // check if video stream device is opened success or not
     // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a9d2ca36789e7fcfe7a7be3b328038585
-    // if( !check )
-    // {
-    //     std::cerr << "Could not open video device." << std::endl;
-    //     return 1;
-    // }
+    if( !camera.isOpened() )
+    {
+        std::cerr << "Could not open video device." << std::endl;
+        return -1;
+    }
 
     // set propety of the frame
     // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a8c6d8c2d37505b5ca61ffd4bb54e9a7c
     // https://docs.opencv.org/3.4.7/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+    camera.set(2, cv::CAP_ANY);
 
     while ( true )
     {
         // get video frame from stream
         // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a473055e77dd7faa4d26d686226b292c1
         // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a199844fb74226a28b3ce3a39d1ff6765
-        // frame = ......
+        camera.read(frame);
 
         // get size of the video frame
         // https://docs.opencv.org/3.4.7/d3/d63/classcv_1_1Mat.html#a146f8e8dda07d1365a575ab83d9828d1
-        // frame_size = ......
+        frame_size = frame.size();
 
         // transfer color space from BGR to BGR565 (16-bit image) to fit the requirement of the LCD
         // https://docs.opencv.org/3.4.7/d8/d01/group__imgproc__color__conversions.html#ga397ae87e1288a81d2363b61574eb8cab
         // https://docs.opencv.org/3.4.7/d8/d01/group__imgproc__color__conversions.html#ga4e0972be5de079fed4e3a10e24ef5ef0
+        cv::cvtColor(frame, bgr565, cv::COLOR_BGR2BGR565);
 
         // output the video frame to framebufer row by row
         for ( int y = 0; y < frame_size.height; y++ )
@@ -64,10 +69,12 @@ int main ( int argc, const char *argv[] )
             // http://www.cplusplus.com/reference/ostream/ostream/seekp/
 
             // write to the framebuffer by "std::ostream::write()"
-            // you could use "cv::Mat::ptr()" to get the pointer of the corresponding row.
+            ofs.seekp(y * fb_info.xres_virtual * (fb_info.bits_per_pixel / 8));
+
             // you also need to cacluate how many bytes required to write to the buffer
             // http://www.cplusplus.com/reference/ostream/ostream/write/
             // https://docs.opencv.org/3.4.7/d3/d63/classcv_1_1Mat.html#a13acd320291229615ef15f96ff1ff738
+            ofs.write(reinterpret_cast<char*>(bgr565.ptr(y)), frame_size.width * (fb_info.bits_per_pixel / 8));
         }
     }
 
@@ -85,15 +92,19 @@ struct framebuffer_info get_framebuffer_info ( const char *framebuffer_device_pa
 
     // open deive with linux system call "open( )"
     // https://man7.org/linux/man-pages/man2/open.2.html
+    int fd = open(framebuffer_device_path, O_RDWR);
 
     // get attributes of the framebuffer device thorugh linux system call "ioctl()"
     // the command you would need is "FBIOGET_VSCREENINFO"
     // https://man7.org/linux/man-pages/man2/ioctl.2.html
     // https://www.kernel.org/doc/Documentation/fb/api.txt
+    ioctl(fd, FBIOGET_VSCREENINFO, &screen_info);
 
     // put the required attributes in variable "fb_info" you found with "ioctl() and return it."
     // fb_info.xres_virtual = ......
     // fb_info.bits_per_pixel = ......
+    fb_info.xres_virtual = screen_info.xres_virtual;
+    fb_info.bits_per_pixel = screen_info.bits_per_pixel;
 
     return fb_info;
 };
