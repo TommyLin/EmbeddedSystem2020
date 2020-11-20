5b21585f4a7acfe82275de08c1bf5862b145b22b
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-11-20 10:22:40 +0800

    New function set_framebuffer

--- lab2-3/src/auto_play.cpp
+++ lab2-3/src/auto_play.cpp
@@ -8,39 +8,14 @@
 
 struct framebuffer_info
 {
-    uint32_t bits_per_pixel;    // framebuffer depth
-    uint32_t xres_virtual;      // how many pixel in a row in virtual screen
+    uint32_t bits_per_pixel;
+    uint32_t xres_virtual;
 };
-
-struct framebuffer_info get_framebuffer_info(const char *framebuffer_device_path);
-
-int main(int argc, const char *argv[])
-{
-    cv::Mat image, bgr565;
-    cv::Size2f image_size;
-
-    framebuffer_info fb_info = get_framebuffer_info("/dev/fb0");
-    std::ofstream ofs("/dev/fb0");
-
-    image = cv::imread("sample.bmp");
-
-    image_size = image.size();
-
-    cv::cvtColor(image, bgr565, cv::COLOR_BGR2BGR565);
-
-    for (int y = 0; y < image_size.height; y++)
-    {
-        ofs.seekp(y * fb_info.xres_virtual * (fb_info.bits_per_pixel / 8));
-        ofs.write(reinterpret_cast<char*>(bgr565.ptr(y)), image_size.width * (fb_info.bits_per_pixel / 8));
-    }
-
-    return 0;
-}
 
 struct framebuffer_info get_framebuffer_info(const char *framebuffer_device_path)
 {
-    struct framebuffer_info fb_info;        // Used to return the required attrs.
-    struct fb_var_screeninfo screen_info;   // Used to get attributes of the device from OS kernel.
+    struct framebuffer_info fb_info;
+    struct fb_var_screeninfo screen_info;
 
     int fd = open(framebuffer_device_path, O_RDWR);
 
@@ -51,3 +26,37 @@ struct framebuffer_info get_framebuffer_info(const char *framebuffer_device_path
 
     return fb_info;
 };
+
+void set_framebuffer(std::ofstream *ofs, cv::Mat *bgr565, cv::Size2f image_size,
+        framebuffer_info fb_info)
+{
+    for (int y = 0; y < image_size.height; y++) {
+        ofs->seekp(y * fb_info.xres_virtual * (fb_info.bits_per_pixel / 8));
+        ofs->write(reinterpret_cast<char*>(bgr565->ptr(y)),
+                image_size.width * (fb_info.bits_per_pixel / 8));
+    }
+
+}
+
+int main(int argc, const char *argv[])
+{
+    cv::Mat image, bgr565;
+    cv::Size2f image_size;
+    const char *dev = "/dev/fb0";
+
+    framebuffer_info fb_info = get_framebuffer_info(dev);
+    std::ofstream ofs(dev);
+
+    while (1) {
+        image = cv::imread("sample.bmp");
+
+        image_size = image.size();
+
+        cv::cvtColor(image, bgr565, cv::COLOR_BGR2BGR565);
+
+        set_framebuffer(&ofs, &bgr565, image_size, fb_info);
+    }
+
+    return 0;
+}
+
