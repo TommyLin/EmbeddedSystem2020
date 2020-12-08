f41e9adb4f3d3604bc687c386381510e82712d29
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-12-08 23:33:52 +0800

    Display date time on screen

--- project/src/osd.cpp
+++ project/src/osd.cpp
@@ -1,9 +1,16 @@
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+
 #include <opencv2/imgcodecs.hpp>
 
 #include "getfbi.hpp"
 
 using namespace std;
 
+
+#define font    cv::FONT_HERSHEY_SIMPLEX
+#define color   cv::Scalar(0, 255, 25)
 
 int main(int argc, const char *argv[])
 {
@@ -13,12 +20,22 @@ int main(int argc, const char *argv[])
     string text = "Group 19";
     cv::Mat image2, image;// = cv::Mat::zeros(cv::Size(fb_info.xres_virtual, fb_info.yres_virtual), CV_64FC1);
 
-    image = cv::imread("/root/wallpapers/1024x600_000.jpg");
-    cv::resize(image, image, cv::Size(fb_info.xres_virtual, 180), 0, 0, cv::INTER_LINEAR);
+    time_t rawtime;
+    string datetime;
 
-    cv::putText(image, text, cv::Point(20, 40), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 25), 1, cv::LINE_AA);
-    cv::cvtColor(image, image2, cv::COLOR_BGR2BGRA);
-    set_framebuffer(&ofs, &image2, image.size(), fb_info);
+
+    while (1) {
+        time(&rawtime);
+        string datetime = asctime(localtime(&rawtime));
+        std::cout << datetime;
+
+        image = cv::imread("/root/black240x100.jpg");
+        cv::putText(image, text, cv::Point(10, 40), font, 1, color, 1, cv::LINE_AA);
+        cv::putText(image, datetime, cv::Point(10, 75), font, 0.5, color, 1, cv::LINE_AA);
+        cv::cvtColor(image, image2, cv::COLOR_BGR2BGRA);
+        set_framebuffer(&ofs, &image2, image.size(), fb_info);
+        sleep(1);
+    }
 
     return 0;
 }
