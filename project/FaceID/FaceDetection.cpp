8592dd24813a41599bca915e6d225a95f2dd661e
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-12-17 22:29:53 +0800

    Remove trailling white spaces

--- project/FaceID/FaceDetection.cpp
+++ project/FaceID/FaceDetection.cpp
@@ -21,39 +21,39 @@ CascadeClassifier eyes_cascade; // Declare the eyes classifier
 String window_name = "Face detection";
 
 int main(int argc, char *argv[]){
-	/* Open the web camera */
-	VideoCapture capture = VideoCapture(2);
-	Mat frame, image;
-	
-	/** Load cascade classifiers **/
-	if(!face_cascade.load(face_cascade_name))
-		PANIC("Error loading face cascade");
-	if(!eyes_cascade.load(eyes_cascade_name))
-		PANIC("Error loading eyes cascade");
-	
-	/** After the camera is opened **/
-	if(capture.isOpened()){
-		cout<<"Face Detection Started..."<<endl;
+    /* Open the web camera */
+    VideoCapture capture = VideoCapture(2);
+    Mat frame, image;
 
-		for(;;){
-			/* Get image from camera */
-			capture>>frame; 			
-			if(frame.empty())
-				PANIC("Error capture frame");
-			
-			/* Start the face detection function */
-			DetectAndDraw(frame);
-			
-			/** If you press ESC, q, or Q , the process will end **/
-			char ch = (char)waitKey(10);
-			if(ch==27 || ch=='q' || ch=='Q')
-				break;
-		}
-	}
-	else
-		PANIC("Error open camera");
-	
-	return 0;
+    /** Load cascade classifiers **/
+    if(!face_cascade.load(face_cascade_name))
+        PANIC("Error loading face cascade");
+    if(!eyes_cascade.load(eyes_cascade_name))
+        PANIC("Error loading eyes cascade");
+
+    /** After the camera is opened **/
+    if(capture.isOpened()){
+        cout<<"Face Detection Started..."<<endl;
+
+        for(;;){
+            /* Get image from camera */
+            capture>>frame;
+            if(frame.empty())
+                PANIC("Error capture frame");
+
+            /* Start the face detection function */
+            DetectAndDraw(frame);
+
+            /** If you press ESC, q, or Q , the process will end **/
+            char ch = (char)waitKey(10);
+            if(ch==27 || ch=='q' || ch=='Q')
+                break;
+        }
+    }
+    else
+        PANIC("Error open camera");
+
+    return 0;
 }
 
 void DetectAndDraw(Mat frame){
@@ -61,37 +61,37 @@ void DetectAndDraw(Mat frame){
     std::vector<Rect> faces, eyes;
     Mat frame_gray, frame_resize;
     int radius;
-	
+
     /* Convert to gray scale */
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     //imshow("grayscale", frame_gray);
-	
+
     /* Resize the grayscale Image */
     //resize(frame_gray, frame_resize, Size(), 1, 1, INTER_LINEAR);
     //imshow("resize", frame_resize);
-	
+
     /* Histogram equalization */
     equalizeHist(frame_gray, frame_gray);
     //imshow("equalize", frame_gray);
-	
+
     /* Detect faces of different sizes using cascade classifier */
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
-	
+
     /** Draw circles around the faces **/
     for (size_t i = 0; i < faces.size(); i++)
     {
         Point center;
- 
+
         /* Draw rectangular on face */
         rectangle(frame, faces[i], Scalar(255, 0, 0), 3, 8, 0);
 
         Mat faceROI = frame_gray(faces[i]);
 
         /* Detection of eyes int the input image */
-        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 1, CV_HAAR_SCALE_IMAGE, Size(3, 3)); 
-         
+        eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 1, CV_HAAR_SCALE_IMAGE, Size(3, 3));
+
         /** Draw circles around eyes **/
-        for (size_t j = 0; j < eyes.size(); j++) 
+        for (size_t j = 0; j < eyes.size(); j++)
         {
             center.x = cvRound((faces[i].x + eyes[j].x + eyes[j].width*0.5));
             center.y = cvRound((faces[i].y + eyes[j].y + eyes[j].height*0.5));
@@ -99,7 +99,7 @@ void DetectAndDraw(Mat frame){
             circle(frame, center, radius, Scalar(0, 255, 0), 3, 8, 0);
         }
     }
- 
+
     // Show Processed Image with detected faces
     imshow( "Face Detection", frame);
 }
